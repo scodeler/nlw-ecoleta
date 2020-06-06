84afe22e96a0caf6e7601a5994dd3c27342fc6f0
bruno scodeler <bruno@estudiomaquinario.com.br>
2020-06-06 18:00:14 -0300

    app ready

---
+++ web/src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+interface HeaderProps {
+  title?: string;
+}
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header>
+      <h1>Ecoleta</h1>
+    </header>
+  )
+}
+
+export default Header;
